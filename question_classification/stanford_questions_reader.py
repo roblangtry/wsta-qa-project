3f76f7ece481298efa892fc9be0dcd9feb24af6b
Reuben Braithwaite <reuben.braithwaite@mac.com>
2017-05-26 13:11:56 +1000

    Fixed it up to use BasicEntityTagger to tag answers, then match tags to questions

--- question_classification/stanford_questions_reader.py
+++ question_classification/stanford_questions_reader.py
@@ -1,32 +1,52 @@
 from questions_reader import QuestionsReader
-from nltk.tag import StanfordNERTagger
+from entity_tagger import BasicEntityTagger
 import json
 
 class StanfordQuestionsReader(QuestionsReader):
 
-    # __train = 'data/QA_train.json'
+#   Note: Should run on training data, not dev, but dev's smaller, so yeah...
     __train = 'data/QA_dev.json'
+    # __train = 'data/QA_train.json'
     __test = 'data/QA_dev.json'
 
     def __init__(self):
         super(self.__class__, self).__init__()
-        self.__tagger = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
+        answer_documents = self.__get_answer_documents()
+        self.__tagger = BasicEntityTagger(answer_documents)
 
-    def get_training_questions(self):
-        return self.__get_questions(self.__train)
-
-    def __get_questions(self, fp):
-        questions = list()
-        with open(fp) as f:
+    def __get_answer_documents(self):
+        answer_documents = list()
+        qanda_tuples = list()
+        with open(self.__train) as f:
             json_data = json.load(f)
             for d in json_data:
                 qanda = d['qa']
                 for obj in qanda:
-                    # try:
-                    coarse = self.__tagger.tag(obj['answer'])
-                    question = obj['question']
-                    questions.append(tuple((coarse, question)))
-                    # except:
-                        # print "Unable to open a line in {}.".format(fp)
-                break
+                    q = obj['question']
+                    a = obj['answer']
+                    qanda_tuples.append(tuple((q, a)))
+                    answer_documents.append(a)
+        self.__qanda_tuples = qanda_tuples
+        return answer_documents
+
+    def get_training_questions(self):
+        return self.__get_questions()
+
+    def get_test_questions(self):
+        return []
+
+    def __get_questions(self):
+        questions = list()
+        for t in self.__qanda_tuples:
+            q = t[0]
+            a = t[1]
+#           This is a very slow operation
+            tags = self.__tagger.tag(a)
+            if len(tags) > 0:
+                print " "
+                for t in tags:
+                    question = tuple((t[0], q))
+                    questions.append(question)
+                    print question
+                print "------------------------------------------------------------"
         return questions
